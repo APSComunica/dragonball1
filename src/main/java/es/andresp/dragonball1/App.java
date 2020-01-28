29e8803141c86a8296a36ef26771093f1b542b70
Usuario <Usuario@Usuario-VAIO>
2020-01-28 14:53:47 +0100

    FREE MOVIENDOSE

--- src/main/java/es/andresp/dragonball1/App.java
+++ src/main/java/es/andresp/dragonball1/App.java
@@ -4,12 +4,12 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -17,14 +17,14 @@ import javafx.util.Duration;
  * JavaFX App
  */
 public class App extends Application {
-    final short SCENE_HEIGHT = 480;
+    final short SCENE_HEIGHT = 470;
     final short SCENE_WIDTH = 800;
            
     short freePosY = (short)((SCENE_HEIGHT)/2);
     byte freeCurrentSpeed = 4;
-    byte freeDirection = 0;
-    
-    
+    byte freeDirectionY = 0;
+    byte freeDirectionX = 0;
+    short freePosX = (short)(SCENE_WIDTH - SCENE_WIDTH);
     
     
     
@@ -42,55 +42,104 @@ public class App extends Application {
         ImageView imageView1 = new ImageView(image1);
         root.getChildren().add(imageView1);
         
+        
+        //free
         Image image2 = new Image(getClass().getResourceAsStream("/imagenes/normal.png"));
         ImageView imageView2 = new ImageView(image2);
-        imageView2.setX(SCENE_WIDTH - SCENE_WIDTH);
+        imageView2.setX(freePosX);
         imageView2.setY(freePosY);
         root.getChildren().add(imageView2);
         
-        
+  
         
         // CONTROL DEL TECLADO
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            public void handle(final KeyEvent keyEvent) {
-                switch(keyEvent.getCode()) {
-                    case UP:
-                        freeDirection = -1;
-                        break;
-                    case DOWN:
-                        freeDirection = 1;
-                        break;
-                }                
+        scene.setOnKeyPressed((final KeyEvent keyEvent) -> {
+            switch(keyEvent.getCode()) {
+                case UP:
+                    freeDirectionY = -1;
+                    break;
+                case DOWN:
+                    freeDirectionY = 1;
+                    break;  
+                    
+                case RIGHT:
+                    freeDirectionX = 1;
+                    break;  
+                    
+                case LEFT:
+                    freeDirectionX = -1;
+                    break;  
+                
+                    
+                    //CAMBIAR A DORADO
+                case T:
+                     image image3= new Image(getClass().getResourceAsStream("/imagenes/dorado.png"));
+                    break; 
             }
         });
         
+ 
+        // CONTROL DEL TECLADO
+        scene.setOnKeyReleased((final KeyEvent keyEvent) -> {
+            switch(keyEvent.getCode()) {
+                case UP:
+                    freeDirectionY = 0;
+                    break;
+                case DOWN:
+                    freeDirectionY = 0;
+                    break;  
+                    
+                case RIGHT:
+                    freeDirectionX = 0;
+                    break;  
+                    
+                case LEFT:
+                    freeDirectionX = 0;
+                    break; 
+                    
+                case T:
+                     ;
+                    break;    
+            }
+        });
         
-        
-        
-        
-        
+
         Timeline timeline = new Timeline(
             // 0.017 ~= 60 FPS
-            new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent ae) {
-     
-                    // ANIMACIÓN DE Free
-                    imageView2.setY(freePosY);
-                    freePosY += freeCurrentSpeed * freeDirection;
-                    if(freePosY <= 0 || freePosY >= SCENE_HEIGHT) {
-                        freeDirection = 0;
-                    }
-                    if(freePosY <= 0) {
-                        freeDirection = 0;
-                        freePosY = 0;
-                    } else if (freePosY >= SCENE_HEIGHT) {
-                        freeDirection = 0;
-                        freePosY = (short)(SCENE_HEIGHT);
-                    }
+            new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
+                // ANIMACIÓN DE Free
+                imageView2.setY(freePosY);
+                imageView2.setX(freePosX);
+                freePosY += freeCurrentSpeed * freeDirectionY;
+                freePosX += freeCurrentSpeed * freeDirectionX;
+                if(freePosY <= 0 || freePosY >= SCENE_HEIGHT) {
+                    freeDirectionY = 0;
                 }
-            }));
+                if(freePosX <= 0 || freePosX >= SCENE_WIDTH) {
+                    freeDirectionX = 0;
+                }
+                if(freePosY <= 0) {
+                    freeDirectionY = 0;
+                    freePosY = 0;
+                } else if (freePosY >= SCENE_HEIGHT) {
+                    freeDirectionY = 0;
+                    freePosY = (short)(SCENE_HEIGHT);
+                }
+                
+                
+                if(freePosX <= 0) {
+                    freeDirectionX = 0;
+                    freePosX = 0;
+                } else if (freePosX >= SCENE_WIDTH) {
+                    freeDirectionX = 0;
+                    freePosX = (short)(SCENE_WIDTH);
+                }
+                
+                
+        }));
         
-        
+         timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
         
         
     }
